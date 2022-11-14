b77407233d6ead0fd47d7cc32de004f0c659b5d2
luis <up202008462@fe.up.pt>
2022-11-14 18:06:17 +0000

    feat: details on feed page

--- resources/views/pages/feed.blade.php
+++ resources/views/pages/feed.blade.php
@@ -5,6 +5,10 @@
     <div class="row">
         @include('partials.feed.left_nav')
         <div class="col-sm-8">
+            <div class="flex align-content-between mt-5">
+                <a class="button float-right" href="#">Ask a question</a>
+                <h2>Leading Questions</h2>
+            </div>
             @foreach ($questions as $question)
                 @include('partials.feed.question_card', ['question' => $question])
             @endforeach
