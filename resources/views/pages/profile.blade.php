05fe4052cab24ead009935ae0cf5b0b8b3c82290
Igor Rodrigues Diniz <up202000162@fe.up.pt>
2022-11-19 20:20:56 +0000

    pictures added

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="container py-5">
     <div class="row">
-      @include('partials.profile.image')
+      @include('partials.profile.image', ['user' => $user])
       @include('partials.profile.header', ['user' => $user])
       <div class="col-lg-1 mt-4">
         <a class="nav-item p-2" href="{{ url('/settings') }}"> <button>Edit Profile</button></a>
