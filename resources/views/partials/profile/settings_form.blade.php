6e9cdc04e732f1c576cd24aee260177944d3b844
Igor Rodrigues Diniz <up202000162@fe.up.pt>
2022-12-30 04:51:43 -0300

    #43 removed duplicate message in profile settings

--- resources/views/partials/profile/settings_form.blade.php
+++ resources/views/partials/profile/settings_form.blade.php
@@ -1,25 +1,6 @@
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
 <form id="edit-user-form" action="{{ route('update_user_api', ['id' => $user->user_id]) }}" method="POST">
     @csrf
     <div class="card m-5 p-5">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @elseif (session('error'))
-            <div class="alert alert-danger" role="alert">
-                {{ session('error') }}
-            </div>
-        @endif
         <div class="form-group">
             <label>Full Name</label>
             <input type="text" id="edit-full-name" name="name" value="{{$user->name}}">
